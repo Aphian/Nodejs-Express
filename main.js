75fbe4247fe0acf68cdab1f2e53067e5c2e532f4
Sang <rsw7876@naver.com>
2025-03-04 16:16:22 +0900

    nodejs_express

--- main.js
+++ main.js
@@ -3,9 +3,16 @@ var template = require('./lib/template.js');
 var path = require('path');
 var qs = require('querystring');
 var sanitizeHtml = require('sanitize-html');
+
 var express = require('express')
+var bodypasser = require('body-parser');
+const bodyParser = require('body-parser');
+
 // 함수처럼 호출
 var app = express()
+
+// body-parser 가 만들어내는 middelware를 표현한 것임
+app.use(bodyParser.urlencoded({extended: false}));
 
 // application 이라는 객체를 반환함.
 // route 또는 routing -> 사용자들이 경로마다 반환해줘야하는데 방향을 알려주는 것.
@@ -66,18 +73,28 @@ app.get('/create', function(request, response){
   });
 });
 
+// Express - body-parser 이용할수 있음.
 app.post('/create_process', function(request, response){
+  /*
   var body = '';
   request.on('data', function(data){
       body = body + data;
   });
   request.on('end', function(){
-      var post = qs.parse(body);
-      var title = post.title;
-      var description = post.description;
-      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-        response.redirect(`/?id=${title}`);
-      })
+    var post = qs.parse(body);
+    var title = post.title;
+    var description = post.description;
+    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+      response.redirect(`/?id=${title}`);
+    })
+  });
+  */
+  // body-parser 활용
+  var post = request.body;
+  var title = post.title;
+  var description = post.description;
+  fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+    response.redirect(`/?id=${title}`);
   });
 });
 
@@ -107,36 +124,24 @@ app.get('/update/:pageId', function(request, response){
   });
 });
 
-app.post('/update_process', function(request, response){
-  var body = '';
-    request.on('data', function(data){
-        body = body + data;
+app.post('/update_process', function(request, response){  
+  var post = request.body;
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+  fs.rename(`data/${id}`, `data/${title}`, function(error){
+    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+      response.redirect(`/?id=${title}`);
     });
-    request.on('end', function(){
-        var post = qs.parse(body);
-        var id = post.id;
-        var title = post.title;
-        var description = post.description;
-        fs.rename(`data/${id}`, `data/${title}`, function(error){
-          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-            response.redirect(`/?id=${title}`);
-          })
-        });
-    });
+  });
 });
 
 app.post('/delete_process', function(request, response){
-  var body = '';
-  request.on('data', function(data){
-      body = body + data;
-  });
-  request.on('end', function(){
-    var post = qs.parse(body);
-    var id = post.id;
-    var filteredId = path.parse(id).base;
-    fs.unlink(`data/${filteredId}`, function(error){
-      response.redirect(`/`);
-    })
+  var post = request.body;
+  var id = post.id;
+  var filteredId = path.parse(id).base;
+  fs.unlink(`data/${filteredId}`, function(error){
+    response.redirect(`/`);
   });
 });
 
