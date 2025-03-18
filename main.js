d3c0e2e3f55c56a10f57c347ec36bddf3db6c44e
Sang <rsw7876@naver.com>
2025-03-18 17:03:37 +0900

    nodejs_passport_refectoring

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ var bodyParser = require('body-parser');
 var compression = require('compression');
 var session = require('express-session')
 var FileStore = require('session-file-store')(session);
-
 var flash = require('connect-flash');
 
 // 함수처럼 호출
@@ -25,19 +24,6 @@ app.use(session({
   store: new FileStore()
 }))
 
-var authData = {
-    // test data
-    email: 'test@a.a.com',
-    password: '111111',
-    nickname: 'test',
-};
-
-var passport = require('passport'),
-  LocalStrategy = require('passport-local').Strategy;
-
-app.use(passport.initialize());
-app.use(passport.session());
-
 app.use(flash());
 app.get('/flash', function(request, response) {
   // session store 에 저장이 되는 값들.
@@ -51,49 +37,7 @@ app.get('/flash-display', function(request, response) {
   response.send(fmsg);
 });
 
-// session 처리 방법
-// serialize 기능 설치
-passport.serializeUser(function(user, done) {
-  // session data 안에 user.email 이라는 사용자 식별자가 들어감
-  // console.log('serializeUser', user);
-  done(null, user.email);
-});
-
-// 로그인이 되면 페이지가 방문할 때마다 호출됨
-// 사용자의 실제 데이터를 조회함
-// callback 의 id 값을 DB에서 조회를 함
-passport.deserializeUser(function(id, done) {
-  // console.log('deserializeUser', id);
-  done(null, authData);
-});
-
-passport.use(new LocalStrategy(
-  {
-    usernameField: 'email',
-    passwordField: 'password',
-  },
-  // done 함수를 어떻게 출력하냐에 따라 로그인 확인 유무 가능
-  function (username, password, done) {
-    console.log(username, password);
-    if (username === authData.email) {
-      // console.log(1);
-      if (password === authData.password) {
-        // console.log(2);
-        return done(null, authData);
-      } else {
-        // console.log(3);
-        return done(null, false, {
-          message: 'Incorrect password.'
-        });
-      }
-    } else {
-      // console.log(4);
-      return done (null, false, {
-        message: "Incorrect username."
-      });
-    }
-  }
-));
+var passport = require('./lib/passport.js')(app);
 
 // passport 전환
 app.post('/auth/login_process', passport.authenticate('local', {
