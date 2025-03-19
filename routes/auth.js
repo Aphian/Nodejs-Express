c377020f720b2e500fd019624b8f9c8c02cc36e1
Sang <rsw7876@naver.com>
2025-03-19 16:48:03 +0900

    nodejs_join

--- routes/auth.js
+++ routes/auth.js
@@ -56,6 +56,30 @@ module.exports = function(passport) {
         successFlash: true
     }));
 
+    router.get('/register', function(request, response){
+        var fmsg = request.flash();
+        var feedback = '';
+        if (fmsg.error) {
+            feedback = fmsg.error[0];
+        }
+
+        var title = 'WEB - Login';
+        var list = template.list(request.list);
+        var html = template.HTML(title, list, `
+            <div style="color: red;">${feedback}</div>
+            <form action="/auth/register_process" method="post">
+                <p><input type="text" name="email" placeholder="email"></p>
+                <p><input type="password" name="password" placeholder="password"></p>
+                <p><input type="password" name="password2" placeholder="password2"></p>
+                <p><input type="text" name="displayName" placeholder="displayName"</p>
+                <p>
+                    <input type="submit" value="Join">
+                </p>
+            </form>
+        `, '');
+        response.send(html);
+    });
+
     router.get('/logout', function(request, response, next){
         request.logout(function(error) {
             if (error) {
